9a9e322a3c43758c44af05175b29eb42a871817c
Alan Kregness <apkregness@gmail.com>
2017-09-29 21:25:15 -0500

    Added \!genjivoice command

--- pong_bot.py
+++ pong_bot.py
@@ -3,6 +3,8 @@ import asyncio
 from bs4 import BeautifulSoup
 import urllib.request
 import re
+import sys
+from os import listdir
 
 client = discord.Client()
 
@@ -12,6 +14,10 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('--------')
+    if sys.maxsize > 2**32:
+        discord.opus.load_opus('libopus-0.x64.dll')
+    else:
+        discord.opus.load_opus('libopus-0.x86.dll')
 
 @client.event
 async def on_message(message):
@@ -30,6 +36,21 @@ async def on_message(message):
         link = soup.find('a', href = re.compile('heroes-of-the-storm-patch-notes'))['href']
         patch_notes_link = patch_note_base_url + link
         await client.send_message(message.channel, patch_notes_link)
+    if(message.content.startswith('!genjivoice')):
+        author = message.author
+        v_channel = author.voice.voice_channel
+        if(v_channel is not None):
+            voice = await client.join_voice_channel(v_channel)
+            player = voice.create_ffmpeg_player('.voice_lines/Genji/Mada_mada.mp3')
+            player.start()
+            while True:
+                try:
+                    if player.is_done():
+                        await voice.disconnect()
+                        break
+                except:
+                    break;
     if(message.content.startswith('!commands')):
         await client.send_message(message.channel, 'Available commands:\n!ping\n!gameplan\n!whothrew\n!patchnotes')
+
 client.run('MzYzMTEzNDY0NTg0OTk0ODE4.DK8fZw.u69xqQC76fYfozoKkGfZueaUgtc')
