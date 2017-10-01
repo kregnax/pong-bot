e4408d3ff002fe27ab7f03d924eed2a23d50cb80
pust777 <josh.pust@gmail.com>
2017-10-01 18:34:50 -0500

    Add get_hero_patch_notes
    -Added get_hero_patch_notes
    -Added get_hero_list docstring

--- fetch.py
+++ fetch.py
@@ -4,6 +4,7 @@ import re
 import json
 
 def get_hero_list():
+    '''Returns a list of current heroes from battlenet'''
     url = "http://us.battle.net/heroes/en/heroes/#/"
     page = requests.get(url, verify=False)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -17,3 +18,14 @@ def get_hero_list():
     for elem in hero_json:
          hero_list.append(elem['analyticsName'])
     return hero_list
+
+def get_hero_patch_notes(hero, next_cmd=False):
+    '''Input a hero name and return a scraped list of the last change notes for that hero. Might add next note funcaitonality'''
+    if hero.lower() in get_hero_list():
+        url = "https://heroespatchnotes.com/hero/{}.html".format(hero) #abathur.html#patch2017-09-26"
+        page = requests.get(url, verify=False)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        notes_list = []
+        for elem in soup.find("div", class_="panel panel-primary").find_all("li"):
+            notes_list.append(elem.get_text())
+    return notes_list
